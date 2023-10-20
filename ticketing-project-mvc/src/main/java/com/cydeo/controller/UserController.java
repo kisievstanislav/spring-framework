52bb7d924623d2f42028a14e695e38f83ff69912
stank <stas.kis@bk.ru>
2023-10-19 20:00:59 -0400

    Ticketing Project MVC - part 3
    finished user page - update and delete ({username} PathVariable)
    project create page (controller,dto,services,retrieve new obj form)

--- ticketing-project-mvc/src/main/java/com/cydeo/controller/UserController.java
+++ ticketing-project-mvc/src/main/java/com/cydeo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.UserService;
 import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -35,11 +32,46 @@ public class UserController {
 
 
     @PostMapping("/create")
-    public String insertUser(@ModelAttribute("user") UserDTO user){
+    public String insertUser(@ModelAttribute("user") UserDTO user, Model model){
+
         userService.save(user);
+
         return "redirect:/user/create";
 
     }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username,Model model){
+
+        model.addAttribute("user",userService.findById(username));
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+
+        return "/user/update";
+
+    }
+
+    @PostMapping("/update")
+    public String updateUser(UserDTO user){
+
+        userService.update(user);
+
+        return "redirect:/user/create";
+
+    }
+
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
+
+
+
 
 
 
